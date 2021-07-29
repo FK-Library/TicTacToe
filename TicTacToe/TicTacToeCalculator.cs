b4399301ca85cecce45c518f901fc464f2de9452
FK-Library <fatemeh.khodadadi@outlook.com>
2021-07-29 08:26:56 +0100

    added calculator

---
+++ TicTacToe/TicTacToeCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTacToeCalculator : ITicTacToeCalculator
+    {
+        public string Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
